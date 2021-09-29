269165b143d91610d710a684cf1654d99e4b39bc
Svet21 <chuyak.svetlana@gmail.com>
2021-09-29 13:07:17 +0500

    Update

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,5 +1,6 @@
 package ru.netology.rest;
 
+
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -16,6 +17,7 @@ class MobileBankApiTestV1 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                    ;
     }
 }
